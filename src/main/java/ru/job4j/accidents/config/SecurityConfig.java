459b460acc18185355a2ce8c2532ed683d93996b
ftptpf <Nb49Rtdd>
2023-02-18 21:26:34 +0300

    Autowired bean passwordEncoder

--- src/main/java/ru/job4j/accidents/config/SecurityConfig.java
+++ src/main/java/ru/job4j/accidents/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package ru.job4j.accidents.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,9 +10,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-/*@Configuration
-@EnableWebSecurity*/
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     /**
      * Для аутентификации загружаем пользователей из памяти.
@@ -21,7 +25,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder passwordEncoder = passwordEncoder();
         auth.inMemoryAuthentication()
                 .passwordEncoder(passwordEncoder)
                 .withUser("user")
