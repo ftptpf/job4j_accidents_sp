a0021befb9c8ee38ad23300b12d6eba96af4eff6
ftptpf <inboxru@yandex.ru>
2023-02-21 09:43:46 +0300

    Registration page

--- src/main/java/ru/job4j/accidents/controller/RegController.java
+++ src/main/java/ru/job4j/accidents/controller/RegController.java
@@ -3,7 +3,9 @@ package ru.job4j.accidents.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.accidents.model.User;
 import ru.job4j.accidents.repository.AuthorityRepository;
 import ru.job4j.accidents.repository.UserRepository;
@@ -16,6 +18,7 @@ public class RegController {
     private final UserRepository users;
     private final AuthorityRepository authorities;
 
+    @PostMapping("/reg")
     public String regSave(@ModelAttribute User user) {
         user.setEnabled(true);
         user.setPassword(encoder.encode(user.getPassword()));
@@ -24,6 +27,7 @@ public class RegController {
         return "redirect:/login";
     }
 
+    @GetMapping("/reg")
     public String regPage() {
         return "reg";
     }
