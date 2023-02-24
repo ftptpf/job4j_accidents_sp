8ae9e21968db18e89d65bb6b12ac4a7c1c465c70
ftptpf <Nb49Rtdd>
2023-02-24 13:22:24 +0300

    Проверка пользователя с тем же username в базе данных перед регистрацией

--- src/main/java/ru/job4j/accidents/controller/RegController.java
+++ src/main/java/ru/job4j/accidents/controller/RegController.java
@@ -3,12 +3,15 @@ package ru.job4j.accidents.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.accidents.model.User;
 import ru.job4j.accidents.repository.AuthorityRepository;
 import ru.job4j.accidents.repository.UserRepository;
+
+import java.util.Optional;
 
 @Controller
 @AllArgsConstructor
@@ -19,11 +22,16 @@ public class RegController {
     private final AuthorityRepository authorities;
 
     @PostMapping("/reg")
-    public String regSave(@ModelAttribute User user) {
+    public String regSave(@ModelAttribute User user, Model model) {
+        Optional<User> userFromDB = users.findUserByUsername(user.getUsername());
+        if (userFromDB.isPresent()) {
+            String errorMessage = "User with such name is already exist in the database. Try to use other login.";
+            model.addAttribute("errorMessage", errorMessage);
+            return "reg";
+        }
         user.setEnabled(true);
         user.setPassword(encoder.encode(user.getPassword()));
         user.setAuthority(authorities.findByAuthority("ROLE_USER"));
-        users.save(user);
         return "redirect:/login";
     }
 
--- src/main/java/ru/job4j/accidents/repository/UserRepository.java
+++ src/main/java/ru/job4j/accidents/repository/UserRepository.java
@@ -3,5 +3,9 @@ package ru.job4j.accidents.repository;
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.accidents.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
+
+    Optional<User> findUserByUsername(String username);
 }
