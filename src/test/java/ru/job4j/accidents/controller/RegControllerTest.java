ddc58438929c9a83fed81e536cf779bf4378992a
ftptpf <inboxru@yandex.ru>
2023-02-27 09:49:14 +0300

    1. Spring mock [#2096]

--- src/test/java/ru/job4j/accidents/controller/RegControllerTest.java
+++ src/test/java/ru/job4j/accidents/controller/RegControllerTest.java
@@ -1,20 +1,33 @@
 package ru.job4j.accidents.controller;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.MultiValueMapAdapter;
 import ru.job4j.accidents.Main;
+import ru.job4j.accidents.model.User;
+import ru.job4j.accidents.repository.UserRepository;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.verify;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
 class RegControllerTest {
+
+    @MockBean
+    private UserRepository userRepository;
 
     @Autowired
     private MockMvc mockMvc;
@@ -26,5 +39,19 @@ class RegControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("reg"));
+    }
+
+    @Test
+    @WithMockUser
+    void whenRegistrationUser() throws Exception {
+        mockMvc.perform(post("/reg")
+                .param("username", "Vika")
+                .param("password", "111"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("login"));
+        ArgumentCaptor<User> argumentCaptor = ArgumentCaptor.forClass(User.class);
+        asssertThat(argumentCaptor.getValue().getUsername(), is("Vika"));
+
     }
 }
