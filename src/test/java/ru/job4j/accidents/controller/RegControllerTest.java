e6a25b6bab3fb9d4c4c50252e10ecd908f12270f
ftptpf <inboxru@yandex.ru>
2023-02-28 09:33:18 +0300

    Test whenRegistrationUser() argument capture

--- src/test/java/ru/job4j/accidents/controller/RegControllerTest.java
+++ src/test/java/ru/job4j/accidents/controller/RegControllerTest.java
@@ -2,22 +2,19 @@ package ru.job4j.accidents.controller;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.util.MultiValueMap;
-import org.springframework.util.MultiValueMapAdapter;
 import ru.job4j.accidents.Main;
 import ru.job4j.accidents.model.User;
 import ru.job4j.accidents.repository.UserRepository;
-import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.verify;
+
 import static org.assertj.core.api.Assertions.*;
 
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -52,11 +49,9 @@ class RegControllerTest {
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/login"));
 
-        ArgumentCaptor<User> argumentCaptor = ArgumentCaptor.forClass(User.class);
-        verify(userRepository).save(argumentCaptor.capture());
-        assertThat(argumentCaptor.getValue().getUsername()).isEqualTo("Vika");
-
-        userRepository.deleteAll();
-
+        ArgumentCaptor<User> argument = ArgumentCaptor.forClass(User.class);
+        verify(userRepository).save(argument.capture());
+        assertThat(argument.getValue().getUsername()).isEqualTo("Vika");
     }
+
 }
